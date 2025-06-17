0faea04683a3e5ae1074de38b79aec5d305a81df
Nasru <nasibov2005@yandex.ru>
2025-06-17 17:46:12 +0300

    optimization

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
+from goods.models import Products
+
 
 def catalog(request):
 
+    goods = Products.objects.all()
+
     context = {
         "title": "Kosyakino - Каталог",
-        "goods": {},
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context)
 
 
 def product(request):
-    context = {}
+    context = {
+        "title": "Kosyakino - Каталог",
+    }
     return render(request, "goods/product.html", context)
