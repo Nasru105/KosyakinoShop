8e86d9efddef4994910d907b8195eb3c39b431be
Nasru <nasibov2005@yandex.ru>
2025-06-19 14:59:04 +0300

    slug conventer for product

--- app/goods/views.py
+++ app/goods/views.py
@@ -14,8 +14,12 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
     context = {
         "title": "Kosyakino - Каталог",
+        "product": product,
     }
     return render(request, "goods/product.html", context)
