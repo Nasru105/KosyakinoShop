c937fe0cf964def63daa7ec4dc302ffe2a4b0247
Nasru <nasibov2005@yandex.ru>
2025-06-19 16:31:41 +0300

    the first pagination option

--- app/goods/models.py
+++ app/goods/models.py
@@ -15,6 +15,7 @@ class Categories(models.Model):
         db_table = "category"
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+        ordering = ("id",)
 
     def __str__(self):
         return self.name
@@ -54,6 +55,7 @@ class Products(models.Model):
         db_table = "product"
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+        ordering = ("id",)
 
     def __str__(self):
         return self.name
