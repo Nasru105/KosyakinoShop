b4002609efdb66946e80179b9cde9ea14507fe08
Nasru <nasibov2005@yandex.ru>
2025-06-22 15:46:54 +0300

    login form and controller

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT
+from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Kosyakino",
+        "NAME": "kosyakino",
         "USER": "kosyakino",
         "PASSWORD": "2345",
         "HOST": "localhost",
@@ -144,3 +144,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
