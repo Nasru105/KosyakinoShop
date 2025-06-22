ca9142efff91b01842723ab3507b9de28d95bafb
Nasru <nasibov2005@yandex.ru>
2025-06-22 11:56:24 +0300

    postgresql connected

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "debug_toolbar",
     "main",
     "goods",
@@ -80,8 +81,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Kosyakino",
+        "USER": "kosyakino",
+        "PASSWORD": "2345",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
