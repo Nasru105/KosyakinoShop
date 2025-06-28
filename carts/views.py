c383ebdd86d5b9aa24ce64be1feba9dbcdfd1175
Nasru <nasibov2005@yandex.ru>
2025-06-28 15:27:50 +0300

    change cart ajax and json response

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,4 @@
+from turtle import update
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
@@ -23,8 +24,8 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    user_carts = get_user_carts(request)
+    cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_carts}, request=request)
     response_data = {
         "message": "Товар добавлен в корзину",
         "cart_items_html": cart_items_html,
@@ -35,9 +36,21 @@ def cart_add(request):
 
 def cart_change(request):
 
-    context = {}
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+    cart = Cart.objects.get(id=cart_id)
 
-    return render(request, "carts/cart_change.html", context)
+    cart.quantity = quantity
+    cart.save()
+
+    user_carts = get_user_carts(request)
+    cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_carts}, request=request)
+    response_data = {
+        "cart_items_html": cart_items_html,
+        "quantity": quantity,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
@@ -47,10 +60,10 @@ def cart_remove(request):
     quantity = cart.quantity
     cart.delete()
 
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    user_carts = get_user_carts(request)
+    cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_carts}, request=request)
     response_data = {
-        "message": "Товар добавлен в корзину",
+        "message": "Товар удален из корзины",
         "cart_items_html": cart_items_html,
         "quantity_deleted": quantity,
     }
